1a2a5344c8ba10462a261647511cb073cdaa6d80
Vikramkyadav <121858395+Vikramkyadav@users.noreply.github.com>
2023-01-18 18:14:29 +0530

    Update Holiday.java

--- Holiday.java
+++ Holiday.java
@@ -1,3 +1,13 @@
+//1. The Java class called Holiday is started below. An object of class Holiday
+//represents aholiday during the year. This class has three instance variables:
+//● name, which is a String representing the name of the holiday
+//● day, which is an int representing the day of the month of the holiday
+//● month, which is a String representing the month the holiday is in
+//a) Write a constructor for the class Holiday, which takes a String representing the name,
+//an int representing the day, and a String representing the month as its arguments, and
+//sets the class variables to these values.
+//b) Write a piece of code that creates a Holiday instance with the name “Independence
+//Day”,with the day “4”, and with the month “July”.
 class Holiday
 {
 	String holidayname;
