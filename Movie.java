7d1431e778a59d12f8755036fdf0a0ed41c44b6d
Vikramkyadav <121858395+Vikramkyadav@users.noreply.github.com>
2023-01-18 18:16:07 +0530

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -1,4 +1,16 @@
-public class Movie
+//2. The class Movie is started below. An instance of class Movie represents a film.
+//This classhas the following three class variables:
+//● title, which is a String representing the title of the movie
+//● studio, which is a String representing the studio that made the movie
+//● rating, which is a String representing the rating of the movie (i.e. PG-13, R, etc)
+//a) Write a constructor for the class Movie, which takes a String representing the title of
+//themovie, a String representing the studio, and a String representing the rating as its
+//arguments, and sets the respective class variables to these values.
+//b) Write a second constructor for the class Movie, which takes a String representing the
+//titleof the movie and a String representing the studio as its arguments, and sets the
+//respective class variables to these values, while the class variable rating is set to "PG".
+//c) Write a piece of code that creates an instance of the class Movie with the title
+//“Casino Royale”, the studio “Eon Productions”, and the rating “PG­13”.public class Movie
 {
 	String Title;
 	String Studio;
